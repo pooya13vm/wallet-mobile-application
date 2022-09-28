31d2a1490fd97bae45db9afc3b36f5f6fbfeae27
pooya <pooya13vm@gmail.com>
2022-09-28 18:15:23 +0300

    cards in home screen is made

---
+++ components/cards/CardItem.tsx
@@ -0,0 +1,74 @@
+import { FunctionComponent } from "react";
+import styled from "styled-components/native";
+
+//components
+import { ScreenWidth } from "../shared";
+import { colors } from "../colors";
+import RegularText from "../Texts/RegularText";
+import SmallText from "../Texts/SmallText";
+import card_bg from "./../../assets/background/background_transparent.png";
+
+//types
+import { CardProps } from "./types";
+import { View } from "react-native";
+
+const CardBackground = styled.ImageBackground`
+  height: 75%;
+  width: ${ScreenWidth * 0.67}px;
+  resize-mode: cover;
+  background-color: ${colors.accent};
+  border-radius: 25px;
+  margin-right: 25px;
+  overflow: hidden;
+`;
+const CardTouchable = styled.TouchableHighlight`
+  height: 100%;
+  border-radius: 25px;
+`;
+const TouchableView = styled.View`
+  justify-content: space-between;
+  align-items: center;
+  padding: 30px;
+  flex: 1;
+`;
+const CardRow = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+`;
+const Logo = styled.Image`
+  width: 100%;
+  height: 80%;
+  resize-mode: contain;
+  flex: 1;
+`;
+
+const CardItem: FunctionComponent<CardProps> = (props) => {
+  const handlePress = () => {};
+  return (
+    <CardBackground source={card_bg}>
+      <CardTouchable underlayColor={colors.secondary} onPress={handlePress}>
+        <TouchableView>
+          <CardRow>
+            <RegularText>******{props.accountNo.slice(6, 10)}</RegularText>
+          </CardRow>
+          <CardRow>
+            <View style={{ flex: 3 }}>
+              <SmallText
+                textStyles={{ marginBottom: 5, color: colors.grayLight }}
+              >
+                Total balance
+              </SmallText>
+              <RegularText textStyles={{ fontSize: 19, fontWeight: "bold" }}>
+                ${props.balance}
+              </RegularText>
+            </View>
+            <Logo source={props.logo} />
+          </CardRow>
+        </TouchableView>
+      </CardTouchable>
+    </CardBackground>
+  );
+};
+export default CardItem;
--- screens/Home.tsx
+++ screens/Home.tsx
@@ -2,8 +2,14 @@ import { FunctionComponent } from "react";
 import styled from "styled-components/native";
 import { StatusBar } from "expo-status-bar";
 
+//custom components
 import { Container } from "../components/shared";
 import { colors } from "../components/colors";
+import CardSection from "../components/cards/CardSection";
+
+//images
+import Logo1 from "./../assets/cards/mc.png";
+import Logo2 from "./../assets/cards/visa_white.png";
 
 const HomeContainer = styled(Container)`
   background-color: ${colors.grayLight};
@@ -12,9 +18,34 @@ const HomeContainer = styled(Container)`
 `;
 
 const Home: FunctionComponent = () => {
+  const cardsDate = [
+    {
+      id: 1,
+      accountNo: "02948243858",
+      balance: 20000.12,
+      alias: "Work Debit",
+      logo: Logo1,
+    },
+    {
+      id: 2,
+      accountNo: "89368767957",
+      balance: 47000.67,
+      alias: "Personal Prepaid",
+      logo: Logo2,
+    },
+    {
+      id: 3,
+      accountNo: "93876556678",
+      balance: 7000.42,
+      alias: "School Prepaid",
+      logo: Logo1,
+    },
+  ];
+
   return (
     <HomeContainer>
       <StatusBar style="dark" />
+      <CardSection data={cardsDate} />
     </HomeContainer>
   );
 };
