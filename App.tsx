5c3729446a3739679e81a2f9c36dc982b91f47a5
pooya <pooya13vm@gmail.com>
2022-09-28 12:00:33 +0300

    stack navigation and RootStack file are added

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import AppLoading from "expo-app-loading";
-import { StatusBar } from "expo-status-bar";
-import Welcome from "./screens/Welcome";
 import { useFonts } from "expo-font";
+
+import RootStack from "./navigators/RootStack";
+import Welcome from "./screens/Welcome";
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -13,5 +14,5 @@ export default function App() {
   if (!fontsLoaded) {
     return <AppLoading />;
   }
-  return <Welcome />;
+  return <RootStack />;
 }
